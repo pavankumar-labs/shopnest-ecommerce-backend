6bb7b8b7c6705f4f1948e581dbf72c35cc9b6057
pavan-kumar <pavankumarsiripiredddy@gmail.com>
2026-05-02 20:10:59 -0700

    step2+3: User entity, Role enum, UserRepository

---
+++ src/main/java/com/pavankumar/shopnestecommercebackend/User.java
@@ -0,0 +1,42 @@
+package com.pavankumar.shopnestecommercebackend;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table(name = "users")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "created_at",nullable = false)
+    private LocalDateTime createdAt;
+
+
+
+
+}
