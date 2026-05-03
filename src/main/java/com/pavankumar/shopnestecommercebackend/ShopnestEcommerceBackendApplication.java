f68cb4699a62f5737e573465c7eaa78a4ff03b31
pavan-kumar <pavankumarsiripiredddy@gmail.com>
2026-05-02 17:19:17 -0700

    Initial setup

---
+++ src/main/java/com/pavankumar/shopnestecommercebackend/ShopnestEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pavankumar.shopnestecommercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopnestEcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopnestEcommerceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pavankumar/shopnestecommercebackend/ShopnestEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavankumar.shopnestecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopnestEcommerceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
